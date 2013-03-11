1439c6b35dabf6c801d9aee56f2f65807cad2cde
Mat Marquis <mat@matmarquis.com>
2013-03-11 10:12:45 -0400

    Collapsible nav.

---
+++ scripts/nav.js
@@ -0,0 +1,40 @@
+(function( w, undefined ) {
+	// Enable JS strict mode
+	"use strict";
+
+	var sel  = "toggle-nav",
+		bkpt = "(max-width: 70em)",
+
+		d = document,
+		oEl = d.getElementById( sel ),
+		init = function() {
+			var txt = d.createTextNode( "Nav" ),
+				toggle = d.createElement( "a" ),
+				toggler = function( e ) {
+					var el = e.target,
+						classes = oEl.getAttribute( "class" ) || "",
+						open = !~classes.indexOf( "hide-me" );
+
+					if( open ) {
+						oEl.setAttribute( "class", classes + " hide-me" );
+					} else {
+						oEl.setAttribute( "class", classes.replace( " hide-me", "" ) );
+					}
+					oEl.setAttribute( "aria-hidden", open );
+					e.preventDefault();
+				};
+
+			toggle.setAttribute( "href", "#" + oEl.id );
+			toggle.setAttribute( "class", "nav-toggle" );
+
+			toggle.appendChild( txt );
+			toggle.addEventListener( "click", toggler );
+
+			oEl.setAttribute( "class", oEl.getAttribute( "class" ) || "" + " hide-me toggled-nav" );
+
+			oEl.parentNode.insertBefore( toggle, oEl );
+		};
+
+	init();
+
+}( this ));

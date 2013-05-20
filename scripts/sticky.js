9227a732cba81c9be0a310554dbb59d736d1e10d
Geri Coady <gericoady@gmail.com>
2013-05-20 00:32:51 -0230

    Updated sticky.js

--- scripts/sticky.js
+++ scripts/sticky.js
@@ -1,16 +1,16 @@
-	$(function(){ // document ready
-		if ( !!$( ".sticky" ).offset() ) { // make sure ".sticky" element exists
-			var stickyTop = $( ".sticky" ).offset().top;
-			$(window).scroll(function(){ // scroll event
-				var windowTop = $(window).scrollTop();
+$(function(){ // document ready
+	if ( !!$( ".sticky" ).offset() ) { // make sure ".sticky" element exists
+		var stickyTop = $( ".sticky" ).offset().top;
+		$(window).scroll(function(){ // scroll event
+			var windowTop = $(window).scrollTop();
 
-				if ( stickyTop < windowTop ) {
-					$( ".sticky" ).addClass( "sticky-stuck" );
-				} else {
-					$( ".sticky" ).removeClass( "sticky-stuck" );
-				}
-			});
-		} else {
-			$( ".sticky" ).removeClass( "sticky-stuck" );
-		}
-	});
+			if ( stickyTop < windowTop ) {
+				$( ".sticky" ).addClass( "sticky-stuck" );
+			} else {
+				$( ".sticky" ).removeClass( "sticky-stuck" );
+			}
+		});
+	} else {
+		$( ".sticky" ).removeClass( "sticky-stuck" );
+	}
+});
